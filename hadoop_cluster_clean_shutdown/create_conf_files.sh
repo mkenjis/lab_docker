fab8c20d8f9ade631121100d8b466f09f9559357
marcello <marcello@dcide.com.br>
2021-11-17 14:43:34 -0300

    Fix config_create_files.sh script

--- hadoop_cluster_clean_shutdown/create_conf_files.sh
+++ hadoop_cluster_clean_shutdown/create_conf_files.sh
@@ -6,51 +6,60 @@ echo '    <name>fs.default.name</name>' >>$HADOOP_HOME/etc/hadoop/core-site.xml
 echo '    <value>hdfs://'${HOSTNAME}':9000</value>' >>$HADOOP_HOME/etc/hadoop/core-site.xml
 echo '  </property>' >>$HADOOP_HOME/etc/hadoop/core-site.xml
 echo '  <property>' >>$SPARK_HOME/conf/core-site.xml
-echo '    <name>dfs.permissions</name>' >>$SPARK_HOME/conf/core-site.xml
-echo '    <value>false</value>' >>$SPARK_HOME/conf/core-site.xml
+echo '    <name>hadoop.tmp.dir</name>' >>$SPARK_HOME/conf/core-site.xml
+echo '    <value>/root/hadoop/hdata</value>' >>$SPARK_HOME/conf/core-site.xml
 echo '  </property>' >>$SPARK_HOME/conf/core-site.xml
 echo '</configuration>' >>$HADOOP_HOME/etc/hadoop/core-site.xml
 
 # hdfs-site.xml (HADOOP e SPARK)
 # =============
 echo '<configuration>' >$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-
 echo '  <property>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
 echo '    <name>dfs.replication</name>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <value>1</value>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
+echo '    <value>2</value>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
 echo '  </property>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-
-echo '  <property>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <name>dfs.name.dir</name>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <value>/data/hadoop/hdfs/namenode</value>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <final>true</final>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '  </property>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-
-echo '  <property>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <name>dfs.data.dir</name>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <value>/data/hadoop/hdfs/datanode</value>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '    <final>true</final>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-echo '  </property>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
-
 echo '</configuration>' >>$HADOOP_HOME/etc/hadoop/hdfs-site.xml
 
 # mapred-site.xml (HADOOP)
 # ===============
 echo '<configuration>' >$HADOOP_HOME/etc/hadoop/mapred-site.xml
 echo '  <property>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
-echo '    <name>mapred.job.tracker</name>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
-echo '    <value>'${HOSTNAME}':9001</value>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
+echo '    <name>mapreduce.framework.name</name>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
+echo '    <value>yarn</value>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
 echo '  </property>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
 echo '</configuration>' >>$HADOOP_HOME/etc/hadoop/mapred-site.xml
 
-# yarn-site.xml (HADOOP)
+# yarn-site.yml (HADOOP)
 # =============
 echo '<configuration>' >$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
 echo '  <property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
 echo '    <name>yarn.nodemanager.aux-services</name>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
 echo '    <value>mapreduce_shuffle</value>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
 echo '  </property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+echo '  <property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <name>yarn.nodemanager.aux-services.mapreduce.shuffle.class</name>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <value>org.apache.hadoop.mapred.ShuffleHandler</value>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '  </property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+echo '  <property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <name>yarn.resourcemanager.resource-tracker.address</name>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <value>master:8025</value>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '  </property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+echo '  <property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <name>yarn.resourcemanager.scheduler.address</name>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <value>master:8030</value>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '  </property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
+echo '  <property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <name>yarn.resourcemanager.address</name>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '    <value>master:8040</value>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+echo '  </property>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
 echo '</configuration>' >>$HADOOP_HOME/etc/hadoop/yarn-site.xml
+
 
 # hadoop-env.sh (HADOOP)
 # =============
