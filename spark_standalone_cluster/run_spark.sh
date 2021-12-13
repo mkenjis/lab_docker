ee445c0d4475cab688d03da406a50103afdc077f
marcello <marcello@dcide.com.br>
2021-12-13 17:37:14 -0300

    Fixes on Spark docker files

--- spark_standalone_cluster/run_spark.sh
+++ spark_standalone_cluster/run_spark.sh
@@ -7,7 +7,6 @@ export PATH=$PATH:.:$JAVA_HOME/bin
 export SPARK_HOME=/usr/local/spark-2.3.2-bin-hadoop2.7
 export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin
 
-# create_conf_files.sh
 
 service ssh start
 
@@ -20,10 +19,16 @@ if [ -n "${SPARK_HOST_SLAVES}" ]; then
 
    sleep 30
    
+   # create_conf_files.sh
+   
+   >${SPARK_HOME}/conf/slaves
+   
    for SPARK_HOST in `echo ${SPARK_HOST_SLAVES} | tr ',' ' '`; do
       ssh-keyscan ${SPARK_HOST} >~/.ssh/known_hosts
-	  echo ${SPARK_HOST} >>$SPARK_HOME/conf/slaves
+	  ssh root@${SPARK_HOST} "cat /etc/hostname" >>${SPARK_HOME}/conf/slaves
    done
+   
+   # start Spark master and slaves nodes
    $SPARK_HOME/sbin/start-master.sh
    $SPARK_HOME/sbin/start-slaves.sh
 fi
