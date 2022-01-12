2856eef627edf6557dce264218f1d769200e278d
marcello <marcello@dcide.com.br>
2022-01-12 17:27:13 -0300

    Fix on Kafka dockerfiles

--- kafka/run_kafka.sh
+++ kafka/run_kafka.sh
@@ -5,17 +5,21 @@ export PATH=$PATH:.:$JAVA_HOME/bin
 export KAFKA_HOME=/usr/local/kafka_2.11-2.1.1
 export PATH=$PATH:$KAFKA_HOME/bin
 
-echo ${BROKER_ID} > /data/zookeeper/myid
+if [ -n "${BROKER_ID}" ]; then
 
-create_conf_files.sh
+   echo ${BROKER_ID} > /data/zookeeper/myid
+
+   create_conf_files.sh
 
 #cat $KAFKA_HOME/config/server.properties.template | sed "s/localhost:2181/zkpr:2181/g" >$KAFKA_HOME/config/server.properties
 #echo "" >>$KAFKA_HOME/config/server.properties
 #echo "port=9092" >>$KAFKA_HOME/config/server.properties
 #echo "advertised.host.name = localhost" >>$KAFKA_HOME/config/server.properties
 
-zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >/tmp/zookeeper.log &
+   zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >/tmp/zookeeper.log &
 
-sleep 30
+   sleep 30
 
-kafka-server-start.sh $KAFKA_HOME/config/server.properties >/tmp/kafka.log
+   kafka-server-start.sh $KAFKA_HOME/config/server.properties >/tmp/kafka.log
+   
+fi
