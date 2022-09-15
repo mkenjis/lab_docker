4869e137c763c139e3c00553af6bd82148ceea33
Download PGM <downloadpgm@gmail.com>
2022-09-15 17:54:46 -0300

    Update on Kafka script

--- kafka/create_conf_files.sh
+++ kafka/create_conf_files.sh
@@ -2,8 +2,9 @@
 # =================
 echo '# change this for each broker' >$KAFKA_HOME/config/server.properties
 echo 'broker.id='${BROKER_ID} >>$KAFKA_HOME/config/server.properties
-echo '# change this to the hostname of each broker' >>$KAFKA_HOME/config/server.properties
-echo 'advertised.listeners=PLAINTEXT://'${HOSTNAME}':9092' >>$KAFKA_HOME/config/server.properties
+#echo '# change this to the hostname of each broker' >>$KAFKA_HOME/config/server.properties
+#echo 'advertised.listeners=PLAINTEXT://'${HOSTNAME}':9092' >>$KAFKA_HOME/config/server.properties
+echo 'advertised.host.name='${HOSTNAME} >>$KAFKA_HOME/config/server.properties
 echo '# The ability to delete topics' >>$KAFKA_HOME/config/server.properties
 echo 'delete.topic.enable=true' >>$KAFKA_HOME/config/server.properties
 echo '# Where logs are stored' >>$KAFKA_HOME/config/server.properties
--- kafka/run_kafka.sh
+++ kafka/run_kafka.sh
@@ -5,6 +5,8 @@ export PATH=$PATH:.:$JAVA_HOME/bin
 export KAFKA_HOME=/usr/local/kafka_2.11-2.1.1
 export PATH=$PATH:$KAFKA_HOME/bin
 
+sleep 30
+
 if [ -n "${BROKER_ID}" ]; then
 
    echo ${BROKER_ID} > /data/zookeeper/myid
