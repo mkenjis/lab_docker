96d409cb3c72d3b60b607e58f456c512a83f9951
mkenjis <mkenjis@gmail.com>
2022-09-16 17:37:43 -0300

    Updates in Docker files

--- kafka/run_kafka.sh
+++ kafka/run_kafka.sh
@@ -5,6 +5,9 @@ export PATH=$PATH:.:$JAVA_HOME/bin
 export KAFKA_HOME=/usr/local/kafka_2.11-2.1.1
 export PATH=$PATH:$KAFKA_HOME/bin
 
+
+mkdir -p $KAFKA_HOME/data
+   
 if [ -n "${BROKER_ID}" ]; then
 
    echo ${BROKER_ID} > $KAFKA_HOME/data/myid
