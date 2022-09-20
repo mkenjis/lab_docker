bb94fa5398f3677c9395fb8a2faffd90bb0131c6
mkenjis <mkenjis@gmail.com>
2022-09-20 12:23:03 -0300

    Fixes on Kafka scripts

--- kafka/run_kafka.sh
+++ kafka/run_kafka.sh
@@ -5,17 +5,24 @@ export PATH=$PATH:.:$JAVA_HOME/bin
 export KAFKA_HOME=/usr/local/kafka_2.11-2.1.1
 export PATH=$PATH:$KAFKA_HOME/bin
 
-
-mkdir -p $KAFKA_HOME/data
+sleep 20
    
-if [ -n "${BROKER_ID}" ]; then
+if [ -n "${ZKP_HOST_BROKERS}" ]; then
 
-   echo ${BROKER_ID} > $KAFKA_HOME/data/myid
+   mkdir -p $KAFKA_HOME/data
 
+   echo $(expr ${BROKER_ID} + 1) > $KAFKA_HOME/data/myid
+
+   for ZKP_HOST in `echo ${ZKP_HOST_BROKERS} | tr ',' ' '`; do
+      nslookup ${ZKP_HOST} | tail -2 | sed "s/Address:/${ZKP_HOST}/g" | awk '{print $2 "  " $1}' >>/etc/hosts
+   done
+   
    create_conf_files.sh
 
-   $KAFKA_HOME/bin/zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >/root/zkp.log
+   zookeeper-server-start.sh $KAFKA_HOME/config/zookeeper.properties >/root/zkp.log &
 
-   $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties >/root/kfk.log
+   sleep 20
+
+   kafka-server-start.sh $KAFKA_HOME/config/server.properties >/root/kfk.log &
    
 fi
