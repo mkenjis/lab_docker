8e900ff99f38f29dc3ceff0fd4244796c54acb3b
mkenjis <mkenjis@gmail.com>
2022-09-20 21:16:07 -0300

    Fixes on Kafka scripts

--- kafka/run_kafka.sh
+++ kafka/run_kafka.sh
@@ -5,6 +5,13 @@ export PATH=$PATH:.:$JAVA_HOME/bin
 export KAFKA_HOME=/usr/local/kafka_2.11-2.1.1
 export PATH=$PATH:$KAFKA_HOME/bin
 
+service ssh start
+
+echo -e "Host *\n\tStrictHostKeyChecking no\n\n" > ~/.ssh/config
+ssh-keyscan ${HOSTNAME} >~/.ssh/known_hosts
+ssh-keyscan localhost >>~/.ssh/known_hosts
+ssh-keyscan 0.0.0.0 >>~/.ssh/known_hosts
+
 sleep 20
    
 if [ -n "${ZKP_HOST_BROKERS}" ]; then
@@ -14,7 +21,8 @@ if [ -n "${ZKP_HOST_BROKERS}" ]; then
    echo $(expr ${BROKER_ID} + 1) > $KAFKA_HOME/data/myid
 
    for ZKP_HOST in `echo ${ZKP_HOST_BROKERS} | tr ',' ' '`; do
-      nslookup ${ZKP_HOST} | tail -2 | sed "s/Address:/${ZKP_HOST}/g" | awk '{print $2 "  " $1}' >>/etc/hosts
+      DOCKER_NAME=$(ssh root@${ZKP_HOST} "cat /etc/hostname")
+      nslookup ${DOCKER_NAME} | tail -2 | sed "s/Address:/${ZKP_HOST}/g" | awk '{print $2 "  " $1}' >>/etc/hosts
    done
    
    create_conf_files.sh
